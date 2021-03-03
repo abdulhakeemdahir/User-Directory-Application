c4286f86367966f5e06c9238fc132cf7f781849d
Abdulhakeem Dahir <somlutions@gmail.com>
2021-03-03 08:54:07 -0800

    added initial users to table

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,9 +2,30 @@ function Body({ users }) {
 	return (
 		<div className='card'>
 			<div className='card-body'>
-				<h5 className='card-title'>Special title treatment</h5>
+				<h5 className='card-title'>Users</h5>
 				{users.map(user => (
-					<p className='card-text'>{user.name.first}</p>
+					<div class='table-responsive'>
+						<table class='table'>
+							<thead>
+								<tr>
+									<th scope='col'>Image</th>
+									<th scope='col'>First Name</th>
+									<th scope='col'>Last Name</th>
+									<th scope='col'>Nationality</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>
+										<img src={user.picture.thumbnail} alt='person imge' />
+									</td>
+									<td>{user.name.first}</td>
+									<td>{user.name.last}</td>
+									<td>{user.nat}</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 				))}
 			</div>
 		</div>
