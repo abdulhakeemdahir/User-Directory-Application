d0d346b15368c746dbf1eeb1d22da0193941bd90
Abdulhakeem Dahir <somlutions@gmail.com>
2021-03-03 13:02:08 -0800

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
 			<h1 className='display-4'>User Directory Application!</h1>
 			<p className='lead'>
 				Hello and welcome to the User Directory Application, you can search by
-				name and filter by the first name!
+				name and sort by Alphabetical Order!
 			</p>
 			<hr className='my-4' />
 		</div>
